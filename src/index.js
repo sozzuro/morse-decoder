05235d1d659144cbb3d967c25836d21520e9311f
Denis Radivil <sozzuro@gmail.com>
2020-10-20 16:35:02 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrNumber = [];
+    let wordElement = '';
+    let resultString = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        arrNumber.push(expr.slice(i, i + 10));
+    }
+
+    arrNumber.map(item => {
+        if (item == '**********') {
+            resultString += ' ';
+        } else {
+            let resultWord = '';
+            for (let j = 0; j < item.length; j += 2) {
+                wordElement = item.slice(j, j + 2);
+                switch (wordElement) {
+                    case '10':
+                        resultWord += '.';
+                        break;
+                    case '11':
+                        resultWord += '-';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            resultString += MORSE_TABLE[resultWord];
+        }
+    });
+
+    return resultString;
 }
 
 module.exports = {
